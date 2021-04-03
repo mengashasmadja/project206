ab1e730704c3a377ffa8fea2a7aad485a03a69b9
mengashasmadja <mengashasmadja010@gmail.com>
2021-04-03 19:36:27 +0700

    TUgas 2 web Service

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title> Portofolio </title>
+  
+<meta name="viewport" content="width=device-width, initial-scale=1"><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css'>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css'>
+<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'><link rel="stylesheet" href="./template/style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<!-- bara navigatie -->
+<!-- INTRO section -->
+
+
+
+  <header id="header">
+    
+      
+      <nav class="menu navbar-inverse navbar-fixed-top navbar-expanded">
+        
+        <a class=" navbar-brand" href="#sectionIntro">Mengas</a>
+        <a class=" unselectable" href="#sectionIntro">Intro </a>
+        <a class=" unselectable" href="#sectionAbout">About</a></li>
+        <!-- <a class=" unselectable" href="#">Blog</a></li>-->
+
+       </nav>
+
+
+    
+
+<!--    
+<div href="javascript:" id="fadein_menu" class="navigation ">
+      <ul class="">
+         bara meniu 
+        <li><a class="navbar-brand" href="#sectionIntro">MO</a></li>
+        <li><a class="right underline active unselectable" href="#">Blog</a></li>
+        <li><a class="right underline active unselectable" href="#sectionContact">Contacts</a></li>
+        <li><a class="right underline active unselectable" href="#sectionServices">Services</a></li>
+        <li><a class="right underline active unselectable" href="#sectionWorks">Portfolio</a></li>
+        <li><a class="right underline active unselectable" href="#sectionResume">Resume</a></li>
+        <li><a class="right underline active unselectable" href="#sectionAbout">About</a></li>
+        <li><a class="right underline active unselectable" href="#sectionIntro">Intro </a></li>
+
+
+        <!-- /bara meniu -->
+
+        <!-- icoane social bara meniu  --
+
+    <div class="social-icons-bara ">
+
+      <a href="#"><i class="fa btn-primary fa-facebook fa-lg fa-fw  " aria-hidden="true"></i></a>
+      <a href="#"><i class="fa btn-lg btn-success fa-twitter fa-lg fa-fw" aria-hidden="true"></i></a>
+      <a href="#"><i class="fa btn-danger fa-github fa-lg fa-fw" aria-hidden="true"></i></a>
+      <a href="#"><i class="fa btn-primary fa-linkedin-square fa-lg fa-fw" aria-hidden="true"></i></a>
+
+    </div>
+  
+  !-- /icoane social bara meniu  -->
+
+      </ul>
+    </div>
+
+  </header>
+
+
+  
+
+
+    <!-- /bara navigatie -->
+
+    <!--upButton-->
+ <a href="javascript:" id="return-to-top"><i class="fa fa-angle-up"></i></a>
+    <!--/upButton-->
+
+<section class="gradient" id="sectionIntro">
+    <!-- INTRO section -->
+
+
+    <div class="container">
+      <div class="aboutContainer">
+        <div class="col-md-12 introHeader">
+          <div class="introContent">
+            <div class="introHeading">
+
+
+              <!-- Avatar prima pagina -->
+              <div class="header-title ">
+              
+            
+            <!--Linkedin -->
+                
+                <!-- nor --
+              
+                <div class="cloudA">
+                  <div class="cloud1"></div>
+                  <div class="cloud2"></div>
+                </div>
+-- /nor -->
+              </div>
+
+              <!-- /Avatar prima pagina -->
+<div id="wrapper" class="">
+  
+  
+    
+              <h2 class="animated zoomIn unselectable disableSelection" >I'M </h2>
+    
+  
+  <h1 class="unselectable ">Mengas Janhasmadja</h1>
+    
+  
+              <div class="introTags animated zoomIn unselectable">
+               
+
+              </div>
+   </div>
+  </div>
+
+              <!-- timer-->
+
+
+              <!-- /timer-->
+
+              <div class="mouse">
+                <a class="btn page-scroll" href="#sectionAbout" >
+                <span></span>
+                <div class="fa fa-long-arrow-down" style="color: #0000;"></div>
+                  </a>
+              </div>
+              <br>
+
+            </div>
+          </div>
+
+        </div>
+
+    
+      </div>
+  </div>
+</section>
+
+
+<!-- ABOUT section -->
+
+
+<section   id="sectionAbout">
+
+
+  <div class="maxwidth container-fluid unselectable" style="background: white;">
+
+
+    <div class="sectionTitle card">
+      <h1>About me</h1>
+   
+
+
+    </div>
+
+
+    <div class="container-fluid">
+      
+
+        <div class="aboutme banner banner-update">
+          <p>Saya Mengas Janhasmadja, saya berasal dari Sumbawa </br>
+          Saat ini saya sedang menempuh Pendidikan di Universitas Bumigora semester VI
+
+          </p>
+        </div>
+      </div>
+   
+
+    <div class="aboutContainer row ">
+
+      <div class=" col-md-4 aboutMe aboutProfile  ">
+
+        <h1>Story</h1>
+        <hr>
+        <p>
+          <!-- <span > -->
+         MIN Empang (2007-2012) <br>
+         MtsN Empang (2012-2015) <br>
+         SMAN Empang (2015-2018) <br>
+         Universitas Bumigora (2018-Sekarang)
+
+      
+          <!-- </span> -->
+
+         
+          <h6 style="text-shadow: none; text-decoration: underline;"> 
+
+      </div>
+
+      <div class="col-md-4 aboutMe aboutProfile ">
+
+        <h1>Profile</h1>
+
+        <hr>
+
+        <img class="profileAv" src="./template/IMG_20210120_165102_694.jpg" alt="">
+        <h3>Mengas Janhasmadja</h3>
+        <h4>Web Developer </br>
+      
+
+
+        <div class="profileInfo">
+
+          </hr>
+          </hr>
+          </hr>
+
+          <h5><span class="textBlue">Birthday:</span>30 January</h5>
+          
+          <h5><span class="textBlue">WhatsApp:</span><a href="https://wa.me/62823397878203">6282339787203</a></h5>
+          <h5><span class="textBlue">E-mail:</span><a href="mengashasmadja010@gmail.com">mengashasmadja010@gmail.com</a></h5>
+
+
+        </div>
+      </div>
+
+      <div class="col-md-4 aboutMe aboutSkills  ">
+        <h1>Skills</h1>
+
+        <hr>
+
+        <h4>HTML5 & CSS3</h4>
+        <div class="progress">
+          <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">
+            80%
+          </div>
+        </div>
+
+        <h4>C#</h4>
+        <div class="progress">
+          <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+            60%
+          </div>
+        </div>
+
+        <h4>Javascript</h4>
+        <div class="progress">
+          <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%;">
+            70%
+          </div>
+        </div>
+
+        <h4>SQL</h4>
+        <div class="progress">
+          <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+            60%
+          </div>
+        </div>
+
+        <h4>Python</h4>
+        <div class="progress">
+          <div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">
+            40%
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+  </div>
+
+
+
+</section>
+
+
+
+<!-- partial -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/typed.js/1.1.1/typed.min.js'></script><script  src="./template/script.js"></script>
+
+</body>
+</html>
